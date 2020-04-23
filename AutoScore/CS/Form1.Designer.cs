8722313c42625d1e153a65f1f8cb0228448eb0c7
Huaimin Wang <58552518+1434807795@users.noreply.github.com>
2020-04-23 09:22:16 +0800

    Add files via upload

---
+++ AutoScore/CS/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace CS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Lb1 = new System.Windows.Forms.Label();
+            this.LbOp = new System.Windows.Forms.Label();
+            this.Lb2 = new System.Windows.Forms.Label();
+            this.TextAnswer = new System.Windows.Forms.TextBox();
+            this.BtnNew = new System.Windows.Forms.Button();
+            this.BtnJudge = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.BtnHelp = new System.Windows.Forms.Button();
+            this.BtnScore = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Lb1
+            // 
+            resources.ApplyResources(this.Lb1, "Lb1");
+            this.Lb1.BackColor = System.Drawing.Color.Transparent;
+            this.Lb1.Name = "Lb1";
+            // 
+            // LbOp
+            // 
+            resources.ApplyResources(this.LbOp, "LbOp");
+            this.LbOp.BackColor = System.Drawing.Color.Transparent;
+            this.LbOp.Name = "LbOp";
+            // 
+            // Lb2
+            // 
+            resources.ApplyResources(this.Lb2, "Lb2");
+            this.Lb2.BackColor = System.Drawing.Color.Transparent;
+            this.Lb2.Name = "Lb2";
+            // 
+            // TextAnswer
+            // 
+            resources.ApplyResources(this.TextAnswer, "TextAnswer");
+            this.TextAnswer.Name = "TextAnswer";
+            this.TextAnswer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextAnswer_KeyPress);
+            // 
+            // BtnNew
+            // 
+            resources.ApplyResources(this.BtnNew, "BtnNew");
+            this.BtnNew.Name = "BtnNew";
+            this.BtnNew.UseVisualStyleBackColor = true;
+            this.BtnNew.Click += new System.EventHandler(this.BtnNew_Click);
+            // 
+            // BtnJudge
+            // 
+            resources.ApplyResources(this.BtnJudge, "BtnJudge");
+            this.BtnJudge.Name = "BtnJudge";
+            this.BtnJudge.UseVisualStyleBackColor = true;
+            this.BtnJudge.Click += new System.EventHandler(this.BtnJudge_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            // 
+            // BtnHelp
+            // 
+            resources.ApplyResources(this.BtnHelp, "BtnHelp");
+            this.BtnHelp.Name = "BtnHelp";
+            this.BtnHelp.UseVisualStyleBackColor = true;
+            this.BtnHelp.Click += new System.EventHandler(this.BtnHelp_Click);
+            // 
+            // BtnScore
+            // 
+            resources.ApplyResources(this.BtnScore, "BtnScore");
+            this.BtnScore.Name = "BtnScore";
+            this.BtnScore.UseVisualStyleBackColor = true;
+            this.BtnScore.Click += new System.EventHandler(this.BtnScore_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Name = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 300000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CS.Properties.Resources.background1;
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnScore);
+            this.Controls.Add(this.BtnHelp);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BtnJudge);
+            this.Controls.Add(this.BtnNew);
+            this.Controls.Add(this.TextAnswer);
+            this.Controls.Add(this.Lb2);
+            this.Controls.Add(this.LbOp);
+            this.Controls.Add(this.Lb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lb1;
+        private System.Windows.Forms.Label LbOp;
+        private System.Windows.Forms.Label Lb2;
+        private System.Windows.Forms.TextBox TextAnswer;
+        private System.Windows.Forms.Button BtnNew;
+        private System.Windows.Forms.Button BtnJudge;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button BtnHelp;
+        private System.Windows.Forms.Button BtnScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ AutoScore/CS/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace CS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //定义变量
+        int a, b, i;
+        string op;
+        int result;
+        int[] score = new int[100];
+
+        //随机数
+        Random rnd = new Random();
+        //点击出题按钮事件
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+            a = rnd.Next(9) + 1;
+            b = rnd.Next(9) + 1;
+            int c = rnd.Next(4);
+
+            switch (c)
+            {
+                case 0: op = "+"; result = a + b; break;
+                case 1: op = "-"; result = a - b; break;
+                case 2: op = "*"; result = a * b; break;
+                case 3: op = "/"; result = a / b; break;
+            }
+
+            Lb1.Text = a.ToString();
+            Lb2.Text = b.ToString();
+            LbOp.Text = op;
+
+            TextAnswer.Text = "";
+        }
+        //点击判断按钮事件
+        private void BtnJudge_Click(object sender, EventArgs e)
+        {
+
+            if (TextAnswer.Text == "")
+            {
+                MessageBox.Show("请输入您的答案！");
+            }
+            else
+            {
+                string str = TextAnswer.Text;
+                double d = double.Parse(str);
+                string disp = "" + a + op + b + "=" + str + " ";
+                if (Math.Abs(d - result) < 1e-3)
+                {
+                    disp += "☆";
+                    score[i] = 1;
+                    i++;
+                }
+                else
+                    disp += "╳";
+                listBox1.Items.Add(disp);
+            };
+            BtnNew_Click(null,null);
+        }
+        //点击帮助按钮事件
+        private void BtnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("您好！这是一个自动出题并判分的小工具，它有手动和自动模式，自动模式出题间隔时间为10秒且自动模式会在5分钟后关闭,启用自动模式后需等待10秒方可出现第一道题。它还能计算您所获得的的总分（每答对一题算一分，错误不算分，最多计100分）");
+        }
+        //点击总分按钮事件
+        private void BtnScore_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for ( i = 0; i < score.Length; i++)
+            {
+                sum += score[i];
+            }
+            MessageBox.Show("您总共答对" + sum + "道题，共计" + sum + "分");
+        }
+        //勾选自动出题
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            timer2.Enabled = true;
+            timer2.Start();
+        }
+        //timer1控件，单题计时
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BtnNew_Click(null,null);
+            BtnJudge_Click(null,null);
+        }
+        //勾选手动出题
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer1.Stop();
+            timer2.Enabled = false;
+            timer2.Stop();
+        }
+        //timer2控件，自动出题整体计时
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            radioButton2_CheckedChanged(null,null);
+        }
+        //输入答案后按下Enter事件
+        private void TextAnswer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar==13)
+            {
+                BtnJudge_Click(null,null);
+            }
+        }
+    }
+}
