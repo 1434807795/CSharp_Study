1611c92a1107a2a64294cded28824738f44d50cd
Huaimin Wang <58552518+1434807795@users.noreply.github.com>
2020-04-25 22:16:24 +0800

    Add files via upload

---
+++ Fraction/CS/Fraction.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DS
+{
+    class Fraction
+    {
+        //字段
+        private int numerator;         //分子
+        private int denominator;       //分母
+
+
+        //属性
+        public int Numerator
+        {
+            get
+            {
+                return numerator;
+            }
+            set
+            {
+                this.numerator = value;
+            }
+        }
+        
+        public int Denominator
+        {
+            get
+            {
+                return denominator;
+            }
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("分母不能为0！\n");
+                }
+                else if (value < 0)
+                {
+                    this.denominator = Math.Abs(value);
+                    this.numerator = -1 * numerator;
+                }
+                else
+                {
+                    this.denominator = value;
+                }
+            }
+        }
+
+        public double DecimalValue
+        {
+            set { }
+            get
+            {
+                return ((double)this.numerator / (double)this.denominator);
+            }
+        }
+
+
+        //方法
+        //构造方法,置初值为0
+        public Fraction()
+        {
+            this.numerator = 0;
+            this.denominator = 1;
+        }
+
+        //辗转相除法即欧几里得算法化简分数
+        //两个整数的最大公约数等于其中较小的那个数和两数相除余数的最大公约数。
+        public static Fraction GCD(Fraction F)
+        {
+            int res = 0;
+            int tmp = 0;
+            int a = F.Numerator;
+            int b = F.Denominator;
+            if (a < b)
+            {
+                tmp = b;
+                b = a;
+                a = tmp;
+            }
+            while (b != 0)
+            {
+                res = a % b;
+                a = b;
+                b = res;
+            }
+            res = a;                                       //求出最大公约数
+            F.Numerator = F.Numerator / res;               //化简分子部分
+            F.Denominator = F.Denominator / res;           //化简分母部分
+            return F;
+        }
+
+        //运算符重载
+        public static Fraction operator +(Fraction F1,Fraction F2)
+        {
+            Fraction tem = new Fraction();
+            tem.Denominator = F1.Denominator * F2.Denominator;
+            tem.Numerator = F1.Numerator * F2.Denominator + F1.Denominator * F2.Numerator;
+            return GCD(tem);
+        }
+        public static Fraction operator -(Fraction F1,Fraction F2)
+        {
+            Fraction tem = new Fraction();
+            tem.Denominator = F1.Denominator * F2.Denominator;
+            tem.Numerator = F1.Numerator * F2.Denominator - F1.Denominator * F2.Numerator;
+            return GCD(tem);
+        }
+        public static Fraction operator *(Fraction F1,Fraction F2)
+        {
+            Fraction tem = new Fraction();
+            tem.Numerator = F1.Numerator * F2.Numerator;
+            tem.Denominator = F1.Denominator * F2.Denominator;
+            return GCD(tem);
+        }
+        public static Fraction operator /(Fraction F1,Fraction F2)
+        {
+            Fraction tem = new Fraction();
+            tem.Numerator = F1.Numerator * F2.Denominator;
+            tem.Denominator = F1.Denominator * F2.Numerator;
+            return GCD(tem);
+        }
+        
+        //比较运算符重载
+        public static bool operator <(Fraction F1,Fraction F2)
+        {
+            return F1.DecimalValue < F2.DecimalValue;
+        }
+        public static bool operator >(Fraction F1,Fraction F2)
+        {
+            return F1.DecimalValue > F2.DecimalValue;
+        }
+
+        //重载ToString方法
+        public override string ToString()
+        {
+            return this.Numerator + "/" + this.Denominator;
+        }
+
+        //定义Parse方法
+        public Fraction Parse(string S)
+        {
+            Fraction tem = new Fraction();
+            string[] str = S.Split('/');
+            tem.Numerator = int.Parse(str[0]);
+            tem.Denominator = int.Parse(str[1]);
+            return GCD(tem);
+        }
+
+        //定义输出函数
+        public void Print(Fraction F)
+        {
+            Console.WriteLine(GCD(F));
+        }
+    }
+}
---
+++ Fraction/CS/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            string[] S;
+            Fraction F = new Fraction();
+            Fraction F0 = new Fraction();
+            Fraction temF = new Fraction();
+            Fraction resF = new Fraction();
+            Console.WriteLine("请按“x/y”的形式输入您想输入的分数，例如：-5/75等。");
+            F = F.Parse(Console.ReadLine());
+            Console.WriteLine("您所输入的分数的最简形式为：");
+            F.Print(F);
+            Console.WriteLine("您所输入的分数所对应的小数值为：{0}",F.DecimalValue);
+            Console.ReadKey();
+            Console.WriteLine("请输入您想与之计算和比较的分数（只需一个），格式需要相同！");
+            F0 = F0.Parse(Console.ReadLine());
+            Console.WriteLine("----------下面是四则运算的结果----------");
+            Console.WriteLine("{0} + {1} = {2}",F.ToString(),F0.ToString(), F + F0);
+            Console.WriteLine("{0} - {1} = {2}",F.ToString(),F0.ToString(), F - F0);
+            Console.WriteLine("{0} * {1} = {2}",F.ToString(),F0.ToString(), F * F0);
+            Console.WriteLine("{0} ÷ {1} = {2}",F.ToString(),F0.ToString(), F / F0);
+            Console.ReadKey();
+            Console.WriteLine("----------下面是比较大小的结果----------");
+            if (F > F0)
+            {
+                Console.WriteLine("{0} > {1}",F.ToString(),F0.ToString());
+            }
+            else if(F < F0)
+            {
+                Console.WriteLine("{0} < {1}",F.ToString(),F0.ToString());
+            }
+            else return;
+            Console.ReadKey();
+            Console.WriteLine("----------下面是字符串数组的操作----------");
+            Console.WriteLine("请输入您想输入的分数个数：");
+            int length = int.Parse(Console.ReadLine());
+            S = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine("请输入您的第{0}个分数：",i+1);
+                S[i] = Console.ReadLine();
+                temF = temF.Parse(S[i]);
+                result += temF.DecimalValue;
+                resF += temF;
+            }
+            Console.WriteLine("----------输入完成----------");
+            Console.WriteLine("您所输入的所有的分数的分数和为{0}，小数和为{1}。",resF.ToString(),result);
+        }
+    }
+}
