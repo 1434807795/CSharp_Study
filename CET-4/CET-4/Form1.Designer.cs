5489b4ee669964524ca1667dd02a8335e3f3083a
Huaimin Wang <58552518+1434807795@users.noreply.github.com>
2020-05-18 19:53:34 +0800

    Add files via upload

---
+++ CET-4/CET-4/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CET_4
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_E = new System.Windows.Forms.Label();
+            this.label_C = new System.Windows.Forms.Label();
+            this.label_Tip = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::CET_4.Properties.Resources.play;
+            this.pictureBox1.Location = new System.Drawing.Point(158, 291);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label_E
+            // 
+            this.label_E.BackColor = System.Drawing.Color.Transparent;
+            this.label_E.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_E.Location = new System.Drawing.Point(167, 30);
+            this.label_E.Name = "label_E";
+            this.label_E.Size = new System.Drawing.Size(106, 34);
+            this.label_E.TabIndex = 1;
+            this.label_E.Text = "单词区";
+            // 
+            // label_C
+            // 
+            this.label_C.BackColor = System.Drawing.Color.Transparent;
+            this.label_C.Font = new System.Drawing.Font("华文中宋+TimesNewRoman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_C.Location = new System.Drawing.Point(167, 112);
+            this.label_C.Name = "label_C";
+            this.label_C.Size = new System.Drawing.Size(91, 25);
+            this.label_C.TabIndex = 2;
+            this.label_C.Text = "释义区";
+            // 
+            // label_Tip
+            // 
+            this.label_Tip.AutoSize = true;
+            this.label_Tip.BackColor = System.Drawing.Color.Transparent;
+            this.label_Tip.Font = new System.Drawing.Font("微软雅黑", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_Tip.Location = new System.Drawing.Point(12, 221);
+            this.label_Tip.Name = "label_Tip";
+            this.label_Tip.Size = new System.Drawing.Size(278, 25);
+            this.label_Tip.TabIndex = 3;
+            this.label_Tip.Text = "请输入间隔时间（单位：秒）：";
+            this.label_Tip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(320, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CET_4.Properties.Resources.BackGround;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(432, 403);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_Tip);
+            this.Controls.Add(this.label_C);
+            this.Controls.Add(this.label_E);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CET-4";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_E;
+        private System.Windows.Forms.Label label_C;
+        private System.Windows.Forms.Label label_Tip;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+
+
+    }
+}
+
---
+++ CET-4/CET-4/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CET_4
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        int _t;
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int i = int.Parse(this.textBox1.Text);
+            this.timer1.Interval = i * 1000;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            List<string> English = new List<string>(); 
+            List<string> Chinese = new List<string>(); 
+            StreamReader sw = new StreamReader(@".\College_Grade4.txt", Encoding.Default); 
+            string tem = sw.ReadToEnd(); 
+            string[] group = tem.Split('\n'); 
+            for (int i = 0; i < group.Length; i++)
+            {
+                string[] words = group[i].Trim().Split('\t'); 
+                if (words.Length < 2) 
+                    continue;
+                English.Add(words[0]); 
+                Chinese.Add(words[1]);
+            }
+            
+            if (_t < group.Length)
+            {
+                this.label_E.Text = English[_t];
+                this.label_C.Text = Chinese[_t];
+            }
+            _t++;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("您此次总计记忆了" + _t + "个单词。");
+        }
+    }
+}
