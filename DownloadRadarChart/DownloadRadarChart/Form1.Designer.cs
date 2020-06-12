1714290eadfe5295b736b6823fb2ace16fd94b6b
Huaimin Wang <58552518+1434807795@users.noreply.github.com>
2020-06-13 07:51:29 +0800

    Add files via upload

---
+++ DownloadRadarChart/DownloadRadarChart/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace DownloadRadarChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtp_End = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtp_Start = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.dtp_End);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtp_Start);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(458, 262);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "请选择起止日期";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开始下载";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dtp_End
+            // 
+            this.dtp_End.Location = new System.Drawing.Point(169, 121);
+            this.dtp_End.MaxDate = new System.DateTime(2020, 6, 30, 0, 0, 0, 0);
+            this.dtp_End.MinDate = new System.DateTime(2020, 6, 1, 0, 0, 0, 0);
+            this.dtp_End.Name = "dtp_End";
+            this.dtp_End.Size = new System.Drawing.Size(180, 25);
+            this.dtp_End.TabIndex = 3;
+            this.dtp_End.TabStop = false;
+            this.dtp_End.Value = new System.DateTime(2020, 6, 1, 0, 0, 0, 0);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(22, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "结束日期";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(22, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "开始日期";
+            // 
+            // dtp_Start
+            // 
+            this.dtp_Start.Location = new System.Drawing.Point(169, 36);
+            this.dtp_Start.MaxDate = new System.DateTime(2020, 6, 30, 0, 0, 0, 0);
+            this.dtp_Start.MinDate = new System.DateTime(2020, 6, 1, 0, 0, 0, 0);
+            this.dtp_Start.Name = "dtp_Start";
+            this.dtp_Start.Size = new System.Drawing.Size(180, 25);
+            this.dtp_Start.TabIndex = 0;
+            this.dtp_Start.TabStop = false;
+            this.dtp_Start.Value = new System.DateTime(2020, 6, 1, 0, 0, 0, 0);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 281);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(450, 160);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtp_Start;
+        private System.Windows.Forms.DateTimePicker dtp_End;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ DownloadRadarChart/DownloadRadarChart/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+using System.Net;
+
+namespace DownloadRadarChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            string dataFullPath;
+            //开始时间
+            DateTime DateStart = dtp_Start.Value;
+            //结束时间
+            DateTime DateEnd = dtp_End.Value;
+            if (DateStart > DateEnd)
+            {
+                MessageBox.Show("时间有误，请重新输入！", "警告");
+            }
+            else
+            {
+                textBox1.Text = "开始下载！请稍等……\r\n";
+                do
+                {
+                    TryDownloadData(DateStart, out dataFullPath);
+                    if (TryDownloadData(DateStart, out dataFullPath))
+                    {
+                        i++;
+                        textBox1.Text += "已完成第" + i.ToString("D2") + "张。\r\n";
+                    }
+                    DateStart = DateStart.AddDays(1);
+                } while (DateStart < DateEnd);
+                textBox1.Text += "下载完成，感谢您的使用！";
+            }
+        }
+
+        static private bool TryDownloadData(DateTime queryHour, out string dataFullPath)
+        {
+            //文件路径不带中文
+            string dataSaveDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+            if (!Directory.Exists(dataSaveDir))
+            {
+                Directory.CreateDirectory(dataSaveDir);
+            }
+            // 雷达拼图文件名模板 Z_RADA_C_BABJ_20200603033000_P_DOR_RDCP_R_ACHN.PNG
+            const string fileNameTemplate = "Z_RADA_C_BABJ_{0}{1}_P_DOR_RDCP_R_ACHN.PNG";
+            // 雷达拼图的Url模板 http://image.data.cma.cn/vis/RAD__B0_CR/20200603/Z_RADA_C_BABJ_20200603033000_P_DOR_RDCP_R_ACHN.PNG
+            const string urlTemplate = // @"http://101.201.178.252/vis/RAD__B0_CR/{0}/";
+                //此处用https://
+               @"https://image.data.cma.cn/vis/RAD__B0_CR/{0}/";
+
+            // 构造日期 格式为20161011
+            string dateStr = queryHour.Year + queryHour.Month.ToString("D2") + queryHour.Day.ToString("D2");
+            // 构造查询时间的文件名，包括日期和小时
+            string filename = string.Format(fileNameTemplate, dateStr, queryHour.Hour.ToString("D2") + "0000");
+            string url = string.Format(urlTemplate, dateStr) + filename;
+
+            // 构造完整的文件路径
+            dataFullPath = Path.Combine(dataSaveDir, filename);
+            // 判定文件是否已经下载，如果下载了则直接退出
+            if (File.Exists(dataFullPath)) return true;
+            // 使用WebClient执行下载
+            try
+            {
+                WebClient client = new WebClient();
+                client.DownloadFile(url, dataFullPath);
+                client.Dispose();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionLength = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
+        }
+    }
+}
